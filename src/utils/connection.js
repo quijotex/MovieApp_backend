c73fa8a50b6fa684bb67e93443b62db08d4fd086
Mario Martinez <mariopresiga@gmail.com>
2023-09-08 23:29:22 -0500

    small change

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'postgres' // pick one of 'mysql','sqlite','postgres','mssql',
+  });
 
 module.exports = sequelize;
