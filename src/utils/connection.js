657d4383f636f326e1d60eddd36a2635953e7bb3
Mario Martinez <mariopresiga@gmail.com>
2023-09-10 09:53:45 -0500

    almost8

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,26 +1,6 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-
-let sequelize;
-if (process.env.node_env === "production") {
-  Sequelize = new sequelize(process.env.database_url);
-} else {
-  Sequelize= new sequelize(
-    process.env.postgres_db || "elitypescript",
-    process.env.postgres_user || "eli",
-    "",
-    {
-      host: process.env.psql_host || "localhost",
-      dialect: "postgres",
-      pool: {
-        max: 100,
-        min: 0,
-        idle: 200000,
-        // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
-        acquire: 1000000,
-      },
-    }
-  );
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = sequelize;
