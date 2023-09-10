fc92c08a704125b660bdaee0760b8d0d93168a13
Mario Martinez <mariopresiga@gmail.com>
2023-09-10 01:34:08 -0500

    H

--- src/server.js
+++ src/server.js
@@ -2,17 +2,15 @@ const app = require('./app');
 const sequelize = require('./utils/connection');
 require('./models');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 
 const main = async () => {
     try {
         sequelize.sync();
         console.log("DB connected");
-        app.listen(PORT, "0.0.0.0", function(){
-            console.log(`Server running on port ${PORT}`);
-        });
-        
+        app.listen(PORT);
+        console.log(`Server running on port ${PORT}`);
     } catch (error) {
         console.log(error)
     }
