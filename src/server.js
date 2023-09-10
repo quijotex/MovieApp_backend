9792a6737df1f61db6334d18744391432c495c54
Mario Martinez <mariopresiga@gmail.com>
2023-09-10 00:45:52 -0500

    Reinstalling node_modules

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const app = require('./app');
 const sequelize = require('./utils/connection');
 require('./models');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", function(){});
+
 const main = async () => {
     try {
         sequelize.sync();
